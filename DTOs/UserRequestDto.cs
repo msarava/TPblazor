d9aba069494cdaeb1c101252de89e22d66eaeaed
Ludovic A <ludovic.adach@outlook.fr>
2022-12-07 14:43:06 +0100

    ajout des fonctionnalités ludo

---
+++ DTOs/UserRequestDto.cs
@@ -0,0 +1,9 @@
+namespace BlazorLeBonCoin.DTOs;
+
+public class UserRequestDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }    
+    public string Password { get; set; }
+}
