024fd9b4aef52dd942e29e4002dcad5ae037173c
Ludovic A <ludovic.adach@outlook.fr>
2022-12-08 13:55:55 +0100

    conflit userresponsedto resolu

--- DTOs/UserResponseDto.cs
+++ DTOs/UserResponseDto.cs
@@ -5,11 +5,9 @@ public class UserResponseDto
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-<<<<<<< HEAD
-    public string Email { get; set; }   
-=======
-    public string Email { get; set; }       
->>>>>>> df780059e815b1c968bd61bb072d6b319cf8e483
+
+    public string Email { get; set; }         
+
     public string Role { get; set; }
 
 }
