b21c869a301ba9f69a5046fb17b6073b5c398143
Ludovic A <ludovic.adach@outlook.fr>
2022-12-08 13:08:10 +0100

    login bug resolu

--- DTOs/LoginDto.cs
+++ DTOs/LoginDto.cs
@@ -4,4 +4,5 @@ public class LoginDto
 {
     public int Id { get; set; }
     public  string Token { get; set; }
+    public bool StatusUser { get; set; }
 }
--- DTOs/UserResponseDto.cs
+++ DTOs/UserResponseDto.cs
@@ -6,6 +6,6 @@ public class UserResponseDto
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }   
-    
     public string Role { get; set; }
+
 }
